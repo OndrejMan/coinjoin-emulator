1b438dfd5a234fe404be0d032c1a6d841517a48b
David Rajnoha <david@rajnoha.eu>
2025-07-04 15:15:58 +0200

    feat: Joinmarket Shadowsocks Compatibility
    Added port forwarding to local loopback in joinmarket container so the
    origin IP will be local host and the requests from remote hosts
    (shadowsocks) won't cause troubles for jm deamon

--- containers/joinmarket-client-server/run.sh
+++ containers/joinmarket-client-server/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 # Starts the RPC server on 28183
-python3 /jm/clientserver/scripts/jmwalletd.py > /home/joinmarket/jmwalletd.log 2>&1
+# python3 /jm/clientserver/scripts/jmwalletd.py > /home/joinmarket/jmwalletd.log 2>&1
+# commented so the logs will show in kubernetes pod logs
+
+# ── forward external 28183 → local-loopback 28182 ────────────
+socat TCP-LISTEN:28183,fork,reuseaddr TCP:127.0.0.1:28182 &
+
+# ── launch the wallet daemon ─────────────
+#    * jmwalletd binds to 127.0.0.1 as usual
+exec python3 /jm/clientserver/scripts/jmwalletd.py --port 28182 \
+     >> /home/joinmarket/jmwalletd.log 2>&1
